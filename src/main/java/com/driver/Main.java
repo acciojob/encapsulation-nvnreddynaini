51ab9c6f0b33b458c8b0c04b3ad61ef36e029fbb
VN Reddy Naini <vnreddynaini@VNs-MacBook-Air.local>
2022-12-12 23:30:14 +0530

    encapsulation 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[]){
+        RWOnly obj = new RWOnly();
+        //'name' has a private access in 'com.driver.RWOnly'
+        //obj.name = "venkat";
+        obj.setName("NVN");
+        System.out.println(obj.getName());
+    }
 }
